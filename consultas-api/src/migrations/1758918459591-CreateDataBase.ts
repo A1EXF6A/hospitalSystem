3d81ab5118e0f15836ce56096ee4a308863cbe0e
RanMd <randommid1873@gmail.com>
2025-09-26 15:40:56 -0500

    feat(database): add env.ts files for configuration management
    - Add env.ts files to admin-api and consultas-api for better environment variable handling.
    - Change database connection settings to use env.ts.

---
+++ consultas-api/src/migrations/1758918459591-CreateDataBase.ts
@@ -0,0 +1,14 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class CreateDataBase1758918459591 implements MigrationInterface {
+    name = 'CreateDataBase1758918459591'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE "consultas" ("id" SERIAL NOT NULL, "paciente" character varying NOT NULL, "doctorId" integer NOT NULL, "centroId" integer NOT NULL, "fecha" TIMESTAMP NOT NULL, "notas" character varying, "estado" character varying NOT NULL DEFAULT 'programada', "created_at" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_889a9011f1854a60a6aae1c6d80" PRIMARY KEY ("id"))`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP TABLE "consultas"`);
+    }
+
+}
