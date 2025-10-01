d3c42305ca7893073b58adfb456575c9c255191d
A1EXF6A <aguachi4414@uta.edu.ec>
2025-09-30 19:22:45 -0500

    changes in dashboard

--- frontend/src/components/Dashboard.tsx
+++ frontend/src/components/Dashboard.tsx
@@ -641,7 +641,7 @@ const Dashboard: React.FC = () => {
             <div className="form-row">
               <input
                 type="email"
-                placeholder="Correo electrónico"
+                placeholder="Correo "
                 value={newUsuario.correo}
                 onChange={(e) => setNewUsuario({...newUsuario, correo: e.target.value})}
                 required
